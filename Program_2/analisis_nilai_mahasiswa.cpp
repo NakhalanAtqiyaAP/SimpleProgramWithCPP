6cb6923d8a6f92a10da77e3639a45b75c4358a5b
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2025-12-08 09:50:28 +0700

    program 2 done

--- Program_2/analisis_nilai_mahasiswa.cpp
+++ Program_2/analisis_nilai_mahasiswa.cpp
@@ -18,6 +18,7 @@ struct mahasiswa
     double rata_rata;
     int min;
     int max;
+    int jumlah_tidak_lulus;
 };
 
 mata_kuliah cariMatkul(string nama_matkul, const vector<mata_kuliah> &daftar)
@@ -48,13 +49,54 @@ double averangeNilai(const vector<pair<string, int>> &nilai)
     return static_cast<double>(total) / nilai.size();
 }
 
+int minNilai(const vector<pair<string, int>> &nilai){
+    if(nilai.empty()){
+        return 0;
+    }
+
+  auto min_it = min_element(nilai.begin(), nilai.end(),
+                [](const pair<string, int>& a, const pair<string, int>& b){
+                    return a.second < b.second;
+                });
+    
+        return min_it->second;
+}
+
 int maxNilai(const vector<pair<string, int>> &nilai){
     if(nilai.empty()){
         return 0;
     }
 
-    auto min_it = min_element(nilai.begin(), nilai.end());
-    int min_val = *min_it;
+    auto max_it = max_element(nilai.begin(), nilai.end(),
+               [](const pair<string, int>& a, const pair<string, int>& b){
+                    return a.second < b.second;
+               });
+
+               return max_it->second;
+}
+
+int nilaiKKM(const vector<pair<string, int>> &nilai, const vector<mata_kuliah> &kkm){
+    if(nilai.empty()){
+        return 0;
+    }
+
+    int gagal_kkm = 0;
+
+    for (const auto& nilai_mk : nilai)
+    {
+      string nama_matkul = nilai_mk.first;
+      int nilai_matkul = nilai_mk.second;
+
+      mata_kuliah matkul_data = cariMatkul(nama_matkul, kkm);
+
+      if (matkul_data.kkm != 0 && nilai_matkul < matkul_data.kkm)
+      {
+        gagal_kkm++;
+      }
+      
+    }
+    
+    return gagal_kkm;
     
 }
 
@@ -103,6 +145,11 @@ int main()
             data_baru.nilai_matkul.push_back({mk.nama_matkul, input_nilai});
         }
         data_baru.rata_rata = averangeNilai(data_baru.nilai_matkul);
+        data_baru.min = minNilai(data_baru.nilai_matkul);
+        data_baru.max = maxNilai(data_baru.nilai_matkul);
+        data_baru.jumlah_tidak_lulus = nilaiKKM(data_baru.nilai_matkul, matakuliah);
+        
+
 
         mahasiswa_data.push_back(data_baru);
     }
@@ -113,7 +160,12 @@ int main()
     for (const auto &data : mahasiswa_data)
     {
         cout << "Nama: " << data.nama << endl
-             << "Nilai Rata-rata" << data.rata_rata;
+             << "Nilai Rata-rata :" << data.rata_rata<<endl
+              << "Nilai Max :" << data.max<<endl
+               << "Nilai Min :" << data.min;
+
+        
+        
 
         cout << endl
              << endl;
@@ -122,7 +174,14 @@ int main()
             cout << " - " << mk.first << " : " << mk.second << endl;
         }
     cout << "------------------------------------------" << endl;
+    if (data.jumlah_tidak_lulus == 0)
+        {
+            cout<<"\nKamu lulus disemua mata kuliah";
+        }else{
+            cout<<"\nKamu gagal dengan gagal di "<< data.jumlah_tidak_lulus 
+            <<" Mata kuliah"<<endl;
+        }
     }
-
+    cout << "------------------------------------------" << endl;
     return 0;
 }
