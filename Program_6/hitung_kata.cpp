1a5e8fc1cb03546c4d7b9f72f295ac63207b0753
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2025-12-16 11:13:09 +0700

    Program 6

--- Program_6/hitung_kata.cpp
+++ Program_6/hitung_kata.cpp
@@ -5,12 +5,24 @@ using namespace std;
 void hitungPanjangString(char kalimat[])
 {
     int panjang = 0;
+    int jumlahKata = 0;
+    bool dalamKata = false;
+
   while (kalimat[panjang] != '\0')
   {
+    if (kalimat[panjang] != ' ' && !dalamKata)
+    {
+        jumlahKata++;
+        dalamKata = true;
+    }
+    else if(kalimat[panjang] == ' '){
+        dalamKata = false;
+    }
+    
     panjang++;
   }
 
-  cout<<"Panjang dari string tersebut adalah "<<panjang<<endl;
+  cout<<"Panjang dari string tersebut adalah "<<jumlahKata<<endl;
   
 }
 
