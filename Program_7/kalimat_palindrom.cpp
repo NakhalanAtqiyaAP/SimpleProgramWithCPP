0b428352f90ebd75e47b22502eae02aa00ce0d54
nakhalanatqiyaAP <nakhalanatqiya@gmail.com>
2025-12-16 11:44:20 +0700

    Program 7

---
+++ Program_7/kalimat_palindrom.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+void hitungPanjangString(char kalimat[])
+{
+    int panjang = 0;
+    int jumlahKata = 0;
+    bool dalamKata = false;
+
+  while (kalimat[panjang] != '\0')
+  {
+    if (kalimat[panjang] != ' ' && !dalamKata)
+    {
+        jumlahKata++;
+        dalamKata = true;
+    }
+    else if(kalimat[panjang] == ' '){
+        dalamKata = false;
+    }
+    
+    panjang++;
+  }
+
+  cout<<"Panjang dari string tersebut adalah "<<jumlahKata<<endl;
+}
+
+void pembalikKata(char kalimat[])
+{
+    int panjang = 0;
+    while (kalimat[panjang] != '\0')
+    {
+       panjang++;
+    }
+    cout<<"kalimat balik :";
+    for (int i = panjang - 1; i >= 0; i--)
+    {
+        cout<<kalimat[i];
+    }
+  
+}
+
+void palindrom(char kalimat[]){
+    int panjang = 0;
+    bool isPalindrom = false;
+    while (kalimat[panjang] != '\0')
+    {
+        panjang++;
+    }
+
+    for (int i = 0; i < panjang / 2; i++)
+    {
+        if (kalimat[i] == kalimat[panjang - 1 - i])
+        {
+            isPalindrom = true;
+            continue;
+        }
+        
+    }
+    if (isPalindrom == true)
+    {
+        cout<<"Kalimat tersebut palindrom"<<endl;
+    }else{
+        cout<<"Kalimat tersebut bukan palindrom"<<endl;
+    }
+    
+    
+}
+
+
+
+int main()
+{
+  char kalimat[255];
+  cout<<"Masukan sebuah kalimat: ";
+  cin.getline(kalimat, 255);
+
+  hitungPanjangString(kalimat);
+  pembalikKata(kalimat);
+  cout<<endl;
+  palindrom(kalimat);
+
+
+  return 0;
+}
